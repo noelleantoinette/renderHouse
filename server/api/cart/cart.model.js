cac5fd61f5374a33948084e83e3e581174a93127
AlexanderHolman146 <Alexander.Holman@gmail.com>
2015-08-08 17:41:08 -0400

    changed endpoint directory to match API RESTful, added user router functionality

--- server/api/cart/cart.model.js
+++ server/api/cart/cart.model.js
@@ -1,11 +1,14 @@
 // Bring in all third part libraries
 
 var mongoose = require('mongoose');
-var crypto = require('crypto');
-var shortid = require('shortid'); //this is a module that can generate shorter unique IDs in lieu of the long MongoDB standard _id.  This was found in the workshop Auther and will be implmeneted because...why not?
 
 // Bring in all outside files from within the app
 var db = require('../../../dbInit.js') // this file contains the connection to the database.  db represents a closure over the command mongoose.connect(databaseURI).connection
 
 
-var Cart
+var Cart = new mongoose.Schema{
+	user: {type:objectId, ref:'User', required:true}
+	products:[type:objectId, ref:'Product']
+};
+
+module.exports = db.Model("Cart",Cart)
