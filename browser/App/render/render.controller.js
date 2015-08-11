10d440dda65e09552a7638e213ec6a921ed5cbc4
Mark Hansen <x@markthemark.com>
2015-08-11 19:02:32 -0400

    cleaned up some remnants of parallel view attempt and plugged in threejs directive

--- browser/App/render/render.controller.js
+++ browser/App/render/render.controller.js
@@ -1,5 +1,9 @@
 'use strict';
 
 app.controller('RenderController', function ($scope) {
-	$scope.letters = ['A','B','C'];
+	$scope.canvasWidth = 400;
+    $scope.canvasHeight = 400;
+    $scope.dofillcontainer = true;
+    $scope.scale = 1;
+    $scope.materialType = 'lambert';
 });
