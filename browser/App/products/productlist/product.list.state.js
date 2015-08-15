fc67955227e1cc1069dea314801c0685c46bad34
Mark Hansen <x@markthemark.com>
2015-08-15 09:54:55 -0400

    Updated product rendering and listing

--- browser/App/products/productlist/product.list.state.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-app.config(function ($stateProvider) {
-	$stateProvider.state('products', {
-		url: '/products',
-		templateUrl: '/browser/app/product/list/product.list.html',
-		controller: 'ProductListCtrl'
-	});
-});
--- browser/app/home/home.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-app.controller('HomeController', function ($scope, RenderService) {
-
-    $scope.changeModel = function(){
-    	RenderService.changeModel();
-    }
-
-});
